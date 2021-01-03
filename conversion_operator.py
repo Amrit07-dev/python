2bb7d91dddd3641fe9c4b4227e0b8171331c47bc
amr_exe <60292024+Amrit07-dev@users.noreply.github.com>
2021-01-03 09:22:43 +0545

    Add files via upload

---
+++ conversion_operator.py
@@ -0,0 +1,19 @@
+#implictconversion
+number_integer = 5
+number_float = 1.4
+sum = number_float + number_integer
+print(sum)
+
+#explictconversion
+a = 4
+new_a = float(a)
+print(new_a)
+print(type(a))
+
+#operators and operator precedence known as BODMAS rule
+b = 3**4
+print(b)
+
+#quotient
+h = 14//7
+print(h)
---
+++ variable.py
@@ -0,0 +1,10 @@
+print("hello")
+
+a = "python"
+a = "Python" # variable can be changed at any time
+
+print(a)
+
+i = input("enter something:" )
+print(i)
+
